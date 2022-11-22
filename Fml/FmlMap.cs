bddb9560bc2a32c3a5c12c12fc1eb069072c54fe
EngineerFergus <williamskei97@gmail.com>
2022-11-21 22:40:32 -0500

    Adding final FmlMap class that can take in a file path, read the text, and return a mapped dictionary of the FML file

---
+++ Fml/FmlMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fml
+{
+    public class FmlMap
+    {
+        private readonly Dictionary<string, string> GlobalPairs;
+
+        private readonly Dictionary<string, Dictionary<string, string>> Sections;
+
+        // TODO add ways to scan and parse files.
+    }
+}
